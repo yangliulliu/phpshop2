06c6558f9642a4892cdbe603adf03a442c0c25c6
liuyang1 <liuyang1@bf.com>
2018-07-26 10:03:21 +0800

    1

---
+++ 测试/renjian-banner1.php
@@ -0,0 +1,54 @@
+{
+    "id": "123",
+    "bidid": "99999",
+    "seatbid": [
+        {
+            "seat": "1231",
+            "bid": [
+                {
+                    "id": "12346",
+                    "impid": "2",
+                    "price": 8.88,
+                    "nurl": "",
+                    "ext": {
+                        "ldp": "http://www.baidu.com",
+			"atype":"1",
+			"apkname": "weixin",
+			"package": "http://weixin.package.android",
+			"app_store_id": "http://weixin.package.ios",
+                        "native_mat": [
+                            {
+                                "mtype": 2,
+                                "url": "http://renjian.jpg"
+                            }
+                        ],
+                        "title": "RRRRR111",
+                        "desc": "aaaaaaaaaaaaaaaaaaaaaa",
+                        "pa": [
+                            {
+                                "type_mma": 1,
+                                "url": "pvUrl1"
+                            },
+                            {
+                                "type_mma": 1,
+                                "url": "pvUrl2"
+                            }
+                        ],
+                        "ca": [
+                            {
+                                "type_mma": 1,
+                                "url": "${ADX_CLICK}"
+                            },
+                            {
+                                "type_mma": 1,
+                                "url": "clickUrl2"
+                            }
+                        ],
+                        "mtype": 7,
+                        "tempid": 3
+                    }
+                }
+            ]
+        }
+    ]
+}
